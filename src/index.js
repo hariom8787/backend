487b3c8d5e3ff2f0eaa0ab83258779c7cac1c21f
hariom8787 <srivastavahariom44@gmail.com>
2025-01-08 15:42:56 +0530

    changes 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ dotenv.config({
     path : './env'
 })
 connectDB()
-
+.then(
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on PORT : ${process.env.PORT}`)
+    })
+)
+.catch((err) => {
+    console.log("service connection Failed !!", err)
+})
 
 
 
